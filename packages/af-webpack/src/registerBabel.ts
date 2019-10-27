84f54b1221d22eda791e3b48985bb717a23c7085
wangtianlun <810394177@qq.com>
2019-10-27 23:11:12 +0800

    feat: 添加afWebpack

---
+++ packages/af-webpack/src/registerBabel.ts
@@ -0,0 +1,5 @@
+export default function registerBabel(opts: any) {
+  const { only, ignore, babelPreset, babelPlugins } = opts;
+
+  
+}
--- packages/michelangelo-build-dev/src/registerBabel.ts
+++ packages/michelangelo-build-dev/src/registerBabel.ts
@@ -1,6 +1,8 @@
 import { join, isAbsolute } from 'path';
+import { existsSync } from 'fs';
 import { uniq } from 'lodash';
 import { winPath } from 'michelangelo-utils';
+import registerBabel from 'af-webpack/lib/registerBabel';
 import { getConfigPaths } from 'michelangelo-core/lib/getUserConfig';
 
 let files: any = [];
@@ -14,7 +16,13 @@ export default function({ cwd }: any) {
 
   const only = files.map((f: string) => {
     const fullPath = isAbsolute(f) ? f : join(cwd, f);
-
-    console.log(fullPath);
+    return winPath(fullPath);
   });
+
+  let absSrcPath = join(cwd, 'src');
+  if (!existsSync(absSrcPath)) {
+    absSrcPath = cwd;
+  }
+
+
 }
